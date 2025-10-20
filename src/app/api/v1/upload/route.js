82e19cd5a147a744a33d47922400517ffe043f36
hafsa sarwath patel <hafsasarwathpatel@gmail.com>
2025-10-20 11:47:33 +0530

    uploading api

--- src/app/api/v1/upload/route.js
+++ src/app/api/v1/upload/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { generateUploadUrl } from "@/util/io";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { generateUploadUrl} from "@/util/io";
+// import { generateUploadUrl} from "@/util/io";
 
 const allowedOrigin = process.env.ALLOWED_ORIGIN || "http://localhost:3001";
 
